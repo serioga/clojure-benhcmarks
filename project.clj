056d2484517e4415454a835f04eba1fc1ef83008
serioga <github@sergey.trofimov.name>
2020-01-16 19:48:33 +0100

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -6,14 +6,15 @@
   :dependencies [; clojure
                  [org.clojure/clojure "1.10.1"]
                  ; libs
-                 [cljstache "2.0.1"]
+                 [cljstache "2.0.5"]
                  [comb "0.1.1"]
                  [fleet "0.10.2"]
-                 [hbs "1.0.2"]
+                 [hbs "1.0.3"]
                  [me.shenfeng/mustache "1.1"]
                  [mustache.clojure "0.3.0"]
-                 [org.clojure/core.match "0.3.0-alpha5"]
+                 [org.clojure/core.async "0.7.559"]
+                 [org.clojure/core.match "0.3.0"]
                  [stencil "0.5.0"]
                  [com.github.spullara.mustache.java/compiler "0.9.6"]
                  ; benchmark
-                 [criterium "0.4.4"]])
+                 [criterium "0.4.5"]])
