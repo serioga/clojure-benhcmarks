8ffe2cd20bce03428ff1a2b2aa478a87bffb03cb
serioga <github@sergey.trofimov.name>
2020-01-27 20:48:08 +0100

    transducers, Last elem after two `(map identity)` operations

--- src/clojure_benchmarks/transducers.clj
+++ src/clojure_benchmarks/transducers.clj
@@ -11,7 +11,8 @@
   (when (odd? x) (inc x)))
 
 
-(comment
+(comment ; Transform sequence (filter + map) to vector
+
   (->> (range 10)
     (keep test-transform)
     (into []))
@@ -130,6 +131,75 @@
       (->> '(0 1 2 3 4 5 6 7 8 9)
         (transduce (keep test-transform) conj))))
   #_"Execution time mean : 278,435039 ns"
+
+
+  :comment)
+
+
+(defn last-rf
+  "Reducing function that returns the last value."
+  ([] nil)
+  ([x] x)
+  ([_ x] x))
+
+
+(comment ; Last elem after two `(map identity)` operations
+
+  (do
+    "Apply map twice"
+    (criterium/quick-bench
+      (->> '(0 1 2 3 4 5 6 7 8 9)
+        (map identity)
+        (map identity)
+        (last))))
+  #_"Execution time mean : 3,071817 µs"
+
+  (do
+    "Apply map with comp"
+    (criterium/quick-bench
+      (->> '(0 1 2 3 4 5 6 7 8 9)
+        (map (comp identity identity))
+        (last))))
+  #_"Execution time mean : 1,661235 µs"
+
+  (do
+    "Apply mapv twice"
+    (criterium/quick-bench
+      (->> '(0 1 2 3 4 5 6 7 8 9)
+        (mapv identity)
+        (mapv identity)
+        (last))))
+  #_"Execution time mean : 1,105114 µs"
+
+  (do
+    "Apply mapv with comp"
+    (criterium/quick-bench
+      (->> '(0 1 2 3 4 5 6 7 8 9)
+        (mapv (comp identity identity))
+        (last))))
+  #_"Execution time mean : 816,599792 ns"
+
+  (do
+    "Apply mapv with comp, `peek` instead of `last`"
+    (criterium/quick-bench
+      (->> '(0 1 2 3 4 5 6 7 8 9)
+        (mapv (comp identity identity))
+        (peek))))
+  #_"Execution time mean : 527,704646 ns"
+
+  (do
+    "Transduce map twice"
+    (criterium/quick-bench
+      (->> '(0 1 2 3 4 5 6 7 8 9)
+        (transduce (comp (map identity) (map identity)) last-rf))))
+  #_"Execution time mean : 501,929073 ns"
+
+  (do
+    "Transduce map with comp"
+    (criterium/quick-bench
+      (->> '(0 1 2 3 4 5 6 7 8 9)
+        (transduce (map (comp identity identity)) last-rf))))
+  #_"Execution time mean : 395,786841 ns"
 
 
   :comment)
