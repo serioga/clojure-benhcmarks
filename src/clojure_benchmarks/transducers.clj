4afcf5bab61cc127cf7640011b5ae0b77d281efb
serioga <github@sergey.trofimov.name>
2020-01-27 18:49:37 +0100

    transducers, conj

--- src/clojure_benchmarks/transducers.clj
+++ src/clojure_benchmarks/transducers.clj
@@ -34,11 +34,18 @@
   #_"Execution time mean : 620,709544 ns"
 
   (do
-    "Range (chunked), transducer"
+    "Range (chunked), transduce, into"
     (criterium/quick-bench
       (->> (range 10)
         (into [] (keep test-transform)))))
   #_"Execution time mean : 266,767011 ns"
+
+  (do
+    "Range (chunked), transduce, conj"
+    (criterium/quick-bench
+      (->> (range 10)
+        (transduce (keep test-transform) conj))))
+  #_"Execution time mean : 308,451779 ns"
 
   (do
     "Vector, traditional, into"
@@ -57,11 +64,18 @@
   #_"Execution time mean : 576,970687 ns"
 
   (do
-    "Vector, transducer"
+    "Vector, transduce, into"
     (criterium/quick-bench
       (->> [0 1 2 3 4 5 6 7 8 9]
         (into [] (keep test-transform)))))
   #_"Execution time mean : 353,753392 ns"
+
+  (do
+    "Vector, transduce, conj"
+    (criterium/quick-bench
+      (->> [0 1 2 3 4 5 6 7 8 9]
+        (transduce (keep test-transform) conj))))
+  #_"Execution time mean : 368,914235 ns"
 
   (do
     "List, traditional, into"
@@ -80,11 +94,18 @@
   #_"Execution time mean : 1,067706 µs"
 
   (do
-    "List, transducer"
+    "List, transduce, into"
     (criterium/quick-bench
       (->> '(0 1 2 3 4 5 6 7 8 9)
         (into [] (keep test-transform)))))
   #_"Execution time mean : 267,802853 ns"
+
+  (do
+    "List, transduce, conj"
+    (criterium/quick-bench
+      (->> '(0 1 2 3 4 5 6 7 8 9)
+        (transduce (keep test-transform) conj))))
+  #_"Execution time mean : 278,435039 ns"
 
 
   :comment)
