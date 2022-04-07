7768b3e095714964f4808744365f0ae72c6de5b6
Sergey Trofimov <sergey@trofimov.name>
2022-04-07 11:06:47 +0200

    JSON benchmarks // Add pjson

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [; clojure
-                 [org.clojure/clojure "1.10.3"]
+                 [org.clojure/clojure "1.11.0"]
                  [org.clojure/core.async "1.2.603"]
                  [org.clojure/core.match "1.0.0"]
                  [medley "1.3.0"]
@@ -13,23 +13,27 @@
                  [functionalbytes/sibiro "0.1.5"]
                  [metosin/reitit "0.5.2"]
                  ; json
-                 [cheshire "5.10.1"]
-                 [com.fasterxml.jackson.core/jackson-core "2.12.4"]
-                 [metosin/jsonista "0.3.3"]
+                 [cheshire "5.10.2"]
+                 [com.fasterxml.jackson.core/jackson-core "2.13.2"]
+                 [metosin/jsonista "0.3.5"]
                  [org.clojure/data.json "2.4.0"]
+                 [pjson "0.5.2"]
                  ; string
                  [funcool/cuerdas "2020.03.26-3"]
                  [superstring "3.0.0"]
                  ; transducers
                  [net.cgrand/xforms "0.19.2"]
                  ; templating
-                 [cljstache "2.0.5"]
-                 [com.github.spullara.mustache.java/compiler "0.9.6"]
+                 [cljstache "2.0.6"]
+                 [com.github.spullara.mustache.java/compiler "0.9.10"]
                  [comb "0.1.1"]
+                 [de.ubercode.clostache/clostache "1.4.0"]
                  [fleet "0.10.2"]
                  [hbs "1.0.3"]
                  [me.shenfeng/mustache "1.1"]
                  [mustache.clojure "0.3.0"]
+                 [org.panchromatic/mokuhan "0.1.1"]
+                 [pogonos "0.1.1"]
                  [stencil "0.5.0"]
                  ; benchmark
                  [criterium "0.4.6"]])
--- src/clojure_benchmarks/json.clj
+++ src/clojure_benchmarks/json.clj
@@ -1,7 +1,8 @@
 (ns clojure-benchmarks.json
   (:require [cheshire.core :as cheshire]
             [clojure.data.json :as data-json]
-            [jsonista.core :as jsonista]))
+            [jsonista.core :as jsonista]
+            [pjson.core :as pjson]))
 
 (set! *warn-on-reflection* true)
 
@@ -15,17 +16,20 @@
 (comment
   "Parse JSON string."
 
-  (cheshire/parse-string test-string)                       ; Execution time mean : 1,335766 µs
-  (cheshire/parse-string-strict test-string)                ; Execution time mean : 1,587872 µs
-  (cheshire/parse-string test-string true)                  ; Execution time mean : 1,733945 µs
-  (cheshire/parse-string-strict test-string true)           ; Execution time mean : 1,340044 µs
+  (cheshire/parse-string test-string)             ; Execution time mean : 1,335766 µs
+  (cheshire/parse-string-strict test-string)      ; Execution time mean : 1,587872 µs
+  (cheshire/parse-string test-string true)        ; Execution time mean : 1,733945 µs
+  (cheshire/parse-string-strict test-string true) ; Execution time mean : 1,340044 µs
 
-  (data-json/read-str test-string)                          ; Execution time mean : 845,653038 ns
-  (data-json/read-str test-string :key-fn keyword)          ; Execution time mean : 1,336589 µs
+  (data-json/read-str test-string)                ; Execution time mean : 845,653038 ns
+  (data-json/read-str test-string :key-fn keyword) ; Execution time mean : 1,336589 µs
 
-  (jsonista/read-value test-string)                         ; Execution time mean : 473,904146 ns
+  (jsonista/read-value test-string)               ; Execution time mean : 473,904146 ns
   (jsonista/read-value test-string
                        jsonista/keyword-keys-object-mapper) ; Execution time mean : 522,288559 ns
+
+  (pjson/read-str test-string)                    ; Execution time mean : 170,064847 ns
+
   )
 
 ;•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
@@ -33,11 +37,13 @@
 (comment
   "Generate JSON string."
 
-  (cheshire/generate-string test-data)                      ; Execution time mean : 1,511262 µs
+  (cheshire/generate-string test-data)            ; Execution time mean : 1,511262 µs
 
-  (data-json/write-str test-data)                           ; Execution time mean : 761,501998 ns
+  (data-json/write-str test-data)                 ; Execution time mean : 761,501998 ns
 
-  (jsonista/write-value-as-string test-data)                ; Execution time mean : 275,184350 ns
+  (jsonista/write-value-as-string test-data)      ; Execution time mean : 275,184350 ns
+
+  (pjson/generate-string test-data)               ; Execution time mean : 250,452683 ns
   )
 
 ;•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
